dc09fd01aaca082096427904ab83a5b1f76ea689
Qasim Wani <qasim31wani@gmail.com>
2020-06-21 18:35:19 -0400

    server barebones established

---
+++ server/socket.py
@@ -0,0 +1,10 @@
+import socket
+
+s = socket.socket(socket.AF_NET, socket.SOCK_STREAM) #AF_NET = ipv4, SOCK_STREAM = TCP (streaming socket)
+s.bind((socket.gethostname(), 3141))
+s.listen(5)
+
+while True:
+    clientsocket, address = s.accept()
+    print(f"Connection from {address} has been established")
+    clientsocket.send(bytes("Hello from the server", "utf-8"))
