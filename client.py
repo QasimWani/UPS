60788a5a6a4fe270c8807d4d9ed5541ce17351fd
Qasim <qasim31wani@gmail.com>
2020-06-22 02:23:06 +0100

    cross platform working commit

--- client.py
+++ client.py
@@ -1,4 +1,6 @@
 import socket
+import datetime
+import time
 from playsound import playsound
 
 ###> Local Machine
@@ -7,11 +9,19 @@ MAX_SIZE = 100000
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_NET = ipv4, SOCK_STREAM = TCP (streaming socket)
 s.connect(("172.16.64.29", 3141)) # later on, change hostname param to this machine's IP
+iterator = 1
+LAST_HEARD = datetime.datetime.now()
+print("current date time: ", LAST_HEARD)
 while True:
+    time.sleep(1)
     msg = s.recv(MAX_SIZE) #buffer byte-stream, decoded later
     val = msg.decode("utf-8") == "True"
     if(val):
+        print("Alert Played. Someone knocked!")
         playsound("alert.mp3")
-        print("Alert Played")
+        LAST_HEARD = datetime.datetime.now()
     else:
-        print("Nothing happened")
+        i+= 1
+        if(i%1800 == 0):
+            i = 1
+            print("last heard: ", LAST_HEARD)
--- server.py
+++ server.py
@@ -4,21 +4,21 @@ import time
 ##> Raspberry Pi.
 import RPi.GPIO as GPIO
 
-#GPIO SETUP
-sound = 17
-
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(sound, GPIO.IN)
-GPIO.add_event_detect(sound, GPIO.BOTH, bouncetime=100)  # let us know when the pin goes HIGH or LOW
-            
-GPIO.add_event_callback(sound, callback)  # assign function to GPIO PIN, Run function on change
+### Socket Setup
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_NET = ipv4, SOCK_STREAM = TCP (streaming socket)
 
 s.bind(("172.16.64.29", 3141))
 s.listen(5)
 clientsocket, address = s.accept()
 print(f"Connection from {address} has been established")
+
+#GPIO SETUP
+sound = 17
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(sound, GPIO.IN)
+
 def callback(sound):
         if GPIO.input(sound):
             print("Sound Detected!")
@@ -27,6 +27,9 @@ def callback(sound):
             global_sound = False
         clientsocket.send(bytes(f"{global_sound}", "utf-8"))### modified in the raspian
 
+GPIO.add_event_detect(sound, GPIO.BOTH, bouncetime=100)  # let us know when the pin goes HIGH or LOW
+            
+GPIO.add_event_callback(sound, callback)  # assign function to GPIO PIN, Run function on change
 
 while True:
     time.sleep(2)
