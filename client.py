9931a136108d614b635ca0836f58edf739ccee84
Qasim Wani <qasim31wani@gmail.com>
2020-06-21 20:21:41 -0400

    buffer size increased server-client

--- client.py
+++ client.py
@@ -1,6 +1,10 @@
 import socket
+###> Local Machine
+#True/False = 1 * 1sec * 86400 = 86.4K. But for excess space: 100K
+MAX_SIZE = 100000
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_NET = ipv4, SOCK_STREAM = TCP (streaming socket)
-s.connect((socket.getHostname(), 2718)) # later on, change hostname param to this machine's IP
-msg = s.recv(1024) #buffer byte-stream, decoded later
-print(msg.decode("utf-8"))
+s.connect((socket.gethostname(), 1999)) # later on, change hostname param to this machine's IP
+while True:
+    msg = s.recv(MAX_SIZE) #buffer byte-stream, decoded later
+    print(msg.decode("utf-8"))
--- server.py
+++ server.py
@@ -1,10 +1,15 @@
 import socket
+import time
+import numpy as np
+##> Raspberry Pi.
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_NET = ipv4, SOCK_STREAM = TCP (streaming socket)
-s.bind((socket.gethostname(), 3141))
+s.bind((socket.gethostname(), 1999))
 s.listen(5)
+clientsocket, address = s.accept()
+print(f"Connection from {address} has been established")
+i = 0
 
 while True:
-    clientsocket, address = s.accept()
-    print(f"Connection from {address} has been established")
-    clientsocket.send(bytes("Hello from the server", "utf-8"))
+    time.sleep(2)
+    clientsocket.send(bytes(f"{np.random.randint(0, 9) < 5}", "utf-8"))### modified in the rapian
