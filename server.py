b149868119651505986ac0868dd248795b3caefa
Qasim Wani <qasim31wani@gmail.com>
2020-06-21 20:44:52 -0400

    merged

--- server.py
+++ server.py
@@ -1,8 +1,23 @@
 import socket
 import time
-import numpy as np
-##> Raspberry Pi.
 
+##> Raspberry Pi.
+import RPi.GPIO as GPIO
+
+#GPIO SETUP
+sound = 17
+global_sound = False
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(sound, GPIO.IN)
+GPIO.add_event_detect(sound, GPIO.BOTH, bouncetime=100)  # let us know when the pin goes HIGH or LOW
+def callback(sound):
+        if GPIO.input(sound):
+            print("Sound Detected!")
+            global_sound = True
+            
+GPIO.add_event_callback(sound, callback)  # assign function to GPIO PIN, Run function on chang
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_NET = ipv4, SOCK_STREAM = TCP (streaming socket)
 s.bind((socket.gethostname(), 1999))
 s.listen(5)
@@ -12,4 +27,6 @@ i = 0
 
 while True:
     time.sleep(2)
-    clientsocket.send(bytes(f"{np.random.randint(0, 9) < 5}", "utf-8"))### modified in the raspian
+    clientsocket.send(bytes(f"{global_sound}", "utf-8"))### modified in the raspian
+
+GPIO.cleanup()
