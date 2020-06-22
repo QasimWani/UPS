3c6a3ca168c5a8376868c68f1a5207591bb13893
Qasim Wani <qasim31wani@gmail.com>
2020-06-21 21:12:50 -0400

    changed callback

--- server.py
+++ server.py
@@ -6,16 +6,11 @@ import RPi.GPIO as GPIO
 
 #GPIO SETUP
 sound = 17
-global_sound = False
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(sound, GPIO.IN)
 GPIO.add_event_detect(sound, GPIO.BOTH, bouncetime=100)  # let us know when the pin goes HIGH or LOW
-def callback(sound):
-        if GPIO.input(sound):
-            print("Sound Detected!")
-            global_sound = True
             
 GPIO.add_event_callback(sound, callback)  # assign function to GPIO PIN, Run function on change
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_NET = ipv4, SOCK_STREAM = TCP (streaming socket)
@@ -24,10 +19,16 @@ s.bind(("172.16.64.29", 3141))
 s.listen(5)
 clientsocket, address = s.accept()
 print(f"Connection from {address} has been established")
-i = 0
+def callback(sound):
+        if GPIO.input(sound):
+            print("Sound Detected!")
+            global_sound = True
+        else:
+            global_sound = False
+        clientsocket.send(bytes(f"{global_sound}", "utf-8"))### modified in the raspian
+
 
 while True:
     time.sleep(2)
-    clientsocket.send(bytes(f"{global_sound}", "utf-8"))### modified in the raspian
 
 GPIO.cleanup()
